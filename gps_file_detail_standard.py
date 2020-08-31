830ac32b90516d88268a22a0518b7be11f324f74
liuli <ll382640782@126.com>
2020-08-31 10:58:38 +0800

    add

--- gps_file_detail_standard.py
+++ gps_file_detail_standard.py
@@ -1,20 +1,32 @@
 # 数据格式化
-# files = os.listdir(path1)
-# for filename in files:
-#     portion = os.path.splitext(filename)
-#     if portion[1] == '.dat':
-#         new_name = portion[0] + '.txt'
-#         os.chdir(path1)
-#         os.rename(filename, new_name)
+import os.path
 
-# os.chdir(path1)
-# with open('0727-dynamic-comb.txt', 'r') as file_default:
-#     data_default = file_default.readlines()
-#     print(data_default)
-#     for line in data_default:
-#         new_message = line.split(',')
-#         if new_message[0] == '$GARMC':
-#             new_message[1] = new_message[1] + ',' + 'A'
-#             # print(new_message)
-#             new_message_detail = ','.join(new_message)
-#             print(new_message_detail)
+path1 = "e:/LEAR_GNSS_Test/Comb"
+os.chdir(path1)
+
+
+# .dat文件改为.txt
+def change_name():
+    files = os.listdir(path1)
+    for filename in files:
+        portion = os.path.splitext(filename)
+        if portion[1] == '.dat':
+            new_name = portion[0] + '.txt'
+            os.chdir(path1)
+            os.rename(filename, new_name)
+
+
+def change_standard():
+    with open('12-123-Ref.txt', 'r') as file_default:
+        data_default = file_default.readlines()
+        for line in data_default:
+            new_message = line.split(',')
+            if new_message[0] == '$GARMC':
+                new_message[1] = new_message[1] + ',' + 'A'
+                # print(new_message)
+                new_message_detail = ','.join(new_message)
+                print(new_message_detail)
+
+
+if __name__ == '__main__':
+    change_standard()
